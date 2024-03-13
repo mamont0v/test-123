547a578957810deeb81ba03b5e86929840ffffbe
Valentin Mamontov <70056133+mamont0v@users.noreply.github.com>
2024-03-13 18:05:11 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,6 @@
-#include <iostream>
-#include "functions.h"
-
-using namespace std;
-
-int main(){
-    print_hello();
-    cout << endl;
-    cout << "The factorial of 5 is " << factorial(5) << endl;
-    return 0;
+int main()
+{
+ int a[10];
+ a[10] = 0;
+ return 0;
 }
